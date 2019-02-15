a355284a2e16bec50d0ba884e0fafb9579cc49bb
Nils Herde <mail@hernil.com>
2019-02-15 14:12:57 +0100

    Initial Kotlin2js proof of concept
    Very basic example of JSON from API being rendered to the DOM by use of Kotlin

---
+++ kotlin2js/src/kotlin/Main.kt
@@ -0,0 +1,28 @@
+import org.w3c.dom.HTMLDivElement
+import org.w3c.xhr.XMLHttpRequest
+import kotlin.browser.document
+import kotlin.js.JSON
+
+val MY_API = "https://jsonplaceholder.typicode.com/comments"
+
+fun getAsync(url: String, callback: (String) -> Unit) {
+  val xmlHttp = XMLHttpRequest()
+  xmlHttp.open("GET", url)
+
+  xmlHttp.onload = {
+    if (xmlHttp.readyState == 4.toShort() && xmlHttp.status == 200.toShort()) {
+      callback.invoke(xmlHttp.responseText)
+    }
+  }
+  xmlHttp.send()
+}
+
+
+fun main(args: Array<String>) {
+  val content = document.getElementById("content") as HTMLDivElement
+
+  getAsync(MY_API) { response ->
+    val comments = JSON.parse<Array<Comment>>(response)
+    comments.forEach { content.appendChild(commentHTML(it)) }
+  }
+}
